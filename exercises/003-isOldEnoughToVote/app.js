83bb39c7db66d96aecc483437e49e46bba937e6b
Matias Kamelman <125071787+kamelmat@users.noreply.github.com>
2024-09-05 10:22:50 +0000

    all age passes so far

--- exercises/003-isOldEnoughToVote/app.js
+++ exercises/003-isOldEnoughToVote/app.js
@@ -1,4 +1,7 @@
 function isOldEnoughToVote(age) {
   // your code here
-  
+  if (age >= 18) return true;
+  else return false
 }
+let output = isOldEnoughToVote(22);
+console.log(output)
--- exercises/004-isOldEnoughToDrinkAndDrive/app.js
+++ exercises/004-isOldEnoughToDrinkAndDrive/app.js
@@ -1,4 +1,8 @@
 function isOldEnoughToDrinkAndDrive(age) {
   // your code here
-  
+  return false
 }
+let output = isOldEnoughToDrinkAndDrive(22);
+console.log(output)
+
+// drinking and driving is ilegal and dangerous to yourself and others. 
